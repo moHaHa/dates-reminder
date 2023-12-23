66d1e5582b7cde864431255bd6fa7ea6e621fbb7
Mohammad_Al_Hallaq <mohammad_al_hallaq@unifisolutions.io>
2023-12-23 04:26:21 -0800

    add agent

--- app/src/main/java/com/example/datesreminderapp/AddAgentFragment.java
+++ app/src/main/java/com/example/datesreminderapp/AddAgentFragment.java
@@ -7,58 +7,74 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddAgentFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.datesreminderapp.models.Agent;
+
+
 public class AddAgentFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private Button btn_addAgent;
+    private EditText et_agentName;
+    private EditText et_agentNumber;
+    private EditText et_agentDescription;
     public AddAgentFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddAgentFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static AddAgentFragment newInstance(String param1, String param2) {
         AddAgentFragment fragment = new AddAgentFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_agent, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_agent, container, false);
+
+        btn_addAgent = view.findViewById(R.id.btn_saveAgent);
+        et_agentName = view.findViewById(R.id.et_agentName);
+        et_agentNumber = view.findViewById(R.id.et_agentNumber);
+        et_agentDescription = view.findViewById(R.id.et_agentDescription);
+        /**
+         * Handle On Add Agent Event
+         * with success message, and  error handling
+         */
+        btn_addAgent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = et_agentName.getText().toString();
+                    String number = et_agentNumber.getText().toString();
+                    String description = et_agentDescription.getText().toString();
+
+                    Agent agent = new Agent(-1, name, number, description);
+                    DatabaseHelper dbHelper = new DatabaseHelper(getActivity());
+                    boolean insertSuccessful = dbHelper.insertAgent(agent);
+
+                    if (insertSuccessful) {
+                        Toast.makeText(getActivity(), "Agent added successfully", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getActivity(), "Failed to add agent", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(getActivity(), "An error occurred", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        return view;
     }
+
 }
---
+++ app/src/main/java/com/example/datesreminderapp/DatabaseHelper.java
@@ -0,0 +1,112 @@
+package com.example.datesreminderapp;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.datesreminderapp.models.Agent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    /**
+     * Static Variables
+     * For Easley Manage Agent Table
+     */
+    private static final String DATABASE_NAME = "reminder_app_database";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "agent";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_NUMBER = "number";
+    private static final String COLUMN_DESCRIPTION = "description";
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    /**
+     * Create Agent Table
+     * 1. make query
+     * 2. use execSQL method for create the table
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableQuery = "CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NAME + " TEXT, " +
+                COLUMN_NUMBER + " TEXT, " +
+                COLUMN_DESCRIPTION + " TEXT)";
+        db.execSQL(createTableQuery);
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String dropTableQuery = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(dropTableQuery);
+        onCreate(db);
+    }
+
+
+    /**
+     *  Insert Agent
+     */
+    public boolean insertAgent(Agent agent) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, agent.getName());
+        values.put(COLUMN_NUMBER, agent.getNumber());
+        values.put(COLUMN_DESCRIPTION, agent.getDescription());
+        long newRowId = db.insert(TABLE_NAME, null, values);
+
+        // if newRowId is not -1 so the insert done successfully , but else this mean it's failed to insert
+        // so: return true => success
+        //     return false => failed
+        return newRowId != -1;
+    }
+
+    /**
+     * Get List Of Agents
+     */
+
+    public List<Agent> getAgents() {
+        List<Agent> agents = new ArrayList<>();
+        SQLiteDatabase db = null;
+
+        try {
+            String selectAllQuery = "SELECT * FROM " + TABLE_NAME;
+
+            db = this.getReadableDatabase();
+            Cursor cursor = db.rawQuery(selectAllQuery, null);
+
+            // looping through all rows and adding to list
+            if (cursor.moveToFirst()) {
+                do {
+                    int id = cursor.getInt(0);
+                    String name = cursor.getString(1);
+                    String number = cursor.getString(2);
+                    String description = cursor.getString(3);
+
+                    // Creating new agent object and adding to list
+                    agents.add(new Agent(id, name, number, description));
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception e) {
+            Log.e("DATABASE_ERROR", e.toString());
+        } finally {
+            if (db != null) {
+                db.close();
+            }
+        }
+
+        return agents;
+    }
+}
